fa3ce2978bd7a222335029ee304b92b988d906d5
Murilo Fekete <90637674+MuriloFekete@users.noreply.github.com>
2023-07-24 20:53:19 -0300

    js

---
+++ Modulo-D/aula001.js
@@ -0,0 +1,8 @@
+var velo = 22
+console.log(`A velocidade do seu carro é ${velo}Km/h`)
+
+if (velo > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+} 
+
+console.log(`Dirija sempre usando sinto de segurança`)
